7ac9a3fa201afae22ec853435b385ba00d203bb3
GewoonSibrand <sibrand@vespertool.com>
2022-09-21 17:35:44 +0200

    Init commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <img src="https://i.pinimg.com/736x/db/55/50/db55509da2df0c8a8e905d4328cd5232.jpg">
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
